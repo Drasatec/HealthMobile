94d798514091951440a198f2d9cec0c0e3e98110
Drasat <drasatsoftwarehouse@gmail.com>
2023-04-25 13:23:47 +0200

    init

---
+++ source/DrasatHealthMobile/DrasatHealthMobile/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DrasatHealthMobile;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ source/DrasatHealthMobile/DrasatHealthMobile/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace DrasatHealthMobile;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
